691d3bba5fdd99dd33a9c9edf7a1108cc8816249
Javier Aroche <aroche290@gmail.com>
2017-06-27 23:19:47 -0700

    update(): code cleanup

--- jsx/Console.jsx
+++ jsx/Console.jsx
@@ -8,16 +8,16 @@
 
 /*
  * Console constructor.
- * 
+ *
  * @constructor
  */
 function Console() {
 	this.newDate;
 	this.newTime;
 	this.logType;
-	
+
 	// Receive file path
-	this.write_file = File( File( $.fileName ).parent.parent.toString() + '/tmp/log.txt' );
+	this.write_file = File(File($.fileName).parent.parent.toString() + '/tmp/log.txt');
 	// Clear the log file
 	this.clear();
 }
@@ -27,9 +27,9 @@ function Console() {
  * Handler function to clear the log.txt file
  * @param {string} message to be logged
  */
-Console.prototype.clear = function() {
+Console.prototype.clear = function () {
 	// Open the file
-	this.write_file.open( 'w', undefined, undefined );
+	this.write_file.open('w', undefined, undefined);
 	this.write_file.encoding = "UTF-8";
 	this.write_file.close();
 }
@@ -39,16 +39,16 @@ Console.prototype.clear = function() {
  * Handler function to write a message to the log.txt file
  * @param {string} message to be logged
  */
-Console.prototype.writeToFile = function( message ) {
+Console.prototype.writeToFile = function (message) {
 	// Open the file
-	this.write_file.open( 'a', undefined, undefined );
+	this.write_file.open('a', undefined, undefined);
 	this.write_file.encoding = "UTF-8";
 
 	// Write message to file
 	this.newDate = new Date();
 	this.newTime = this.newDate.getHours() + ':' + this.newDate.getMinutes() + ':' + this.newDate.getSeconds() + '.' + this.newDate.getMilliseconds();
-	
-	this.write_file.writeln('##[' + this.logType + ': ' + this.newTime + '] ' +  message );
+
+	this.write_file.writeln('##[' + this.logType + ': ' + this.newTime + '] ' + message);
 	this.write_file.close();
 }
 
@@ -57,9 +57,9 @@ Console.prototype.writeToFile = function( message ) {
  * Handler function to write a log message to the log.txt file
  * @param {string} message to be logged
  */
-Console.prototype.log = function( message ) {
+Console.prototype.log = function (message) {
 	this.logType = 'log';
-	this.writeToFile( message );
+	this.writeToFile(message);
 }
 
 /*
@@ -67,9 +67,9 @@ Console.prototype.log = function( message ) {
  * Handler function to write a warn message to the log.txt file
  * @param {string} message to be logged
  */
-Console.prototype.warn = function( message ) {
+Console.prototype.warn = function (message) {
 	this.logType = 'warn';
-	this.writeToFile( message );
+	this.writeToFile(message);
 }
 
 /*
@@ -77,9 +77,9 @@ Console.prototype.warn = function( message ) {
  * Handler function to write an error message to the log.txt file
  * @param {string} message to be logged
  */
-Console.prototype.error = function( message ) {
+Console.prototype.error = function (message) {
 	this.logType = 'error';
-	this.writeToFile( message );
+	this.writeToFile(message);
 }
 
 /*
@@ -87,9 +87,9 @@ Console.prototype.error = function( message ) {
  * Handler function to write an info message to the log.txt file
  * @param {string} message to be logged
  */
-Console.prototype.info = function( message ) {
+Console.prototype.info = function (message) {
 	this.logType = 'info';
-	this.writeToFile( message );
+	this.writeToFile(message);
 }
 
 /*
@@ -97,9 +97,9 @@ Console.prototype.info = function( message ) {
  * Handler function to write a stringified message to the log.txt file
  * @param {string} message to be logged
  */
-Console.prototype.stringify = function( message ) {
+Console.prototype.stringify = function (message) {
 	this.logType = 'stringify';
-	this.writeToFile( JSON.stringify( message, null, 4 ) );
+	this.writeToFile(JSON.stringify(message, null, 4));
 }
 
 // Create a new Console instance
--- main.js
+++ main.js
@@ -10,231 +10,231 @@
 /*global define, $, brackets, window */
 
 define(function (require, exports, module) {
-    'use strict';
+	'use strict';
 
-    // Brackets Modules
-    var DocumentManager   = brackets.getModule( 'document/DocumentManager' ),
-        Dialogs           = brackets.getModule( 'widgets/Dialogs' ),
-        AppInit           = brackets.getModule( 'utils/AppInit' ),
-        NodeDomain        = brackets.getModule( 'utils/NodeDomain' ),
-        ExtensionUtils    = brackets.getModule( 'utils/ExtensionUtils' ),
-        WorkspaceManager  = brackets.getModule( 'view/WorkspaceManager' ),
-        CommandManager    = brackets.getModule( 'command/CommandManager' ),
-        KeyBindingManager = brackets.getModule( 'command/KeyBindingManager' );
-    
-    // Load domain
-    var DomainManager_path = ExtensionUtils.getModulePath(module, 'lib/DomainManager');
-    var DomainManager      = new NodeDomain('Br-Ps', DomainManager_path);
-    
-    var panelHTML = require("text!html/console.html"),
-        panel,
-        $panel,
-        $clearBtn,
-        $console,
-        $closeBtn,
-        $psVersion,
-        $executionTime,
-        message;
-        
-    /*
-     * @private
-     * Function to validate and get the properties of the current document
-     */
-    function _validateCurrentDocument() {
-        // Get info of working file
-        var currentDoc = DocumentManager.getCurrentDocument();
-        var currentDocInfo = {
-            path    : currentDoc.file.fullPath,
-            isDirty : currentDoc.isDirty,
-            text    : currentDoc.getText()
-        };
+	// Brackets Modules
+	var DocumentManager = brackets.getModule('document/DocumentManager'),
+		Dialogs = brackets.getModule('widgets/Dialogs'),
+		AppInit = brackets.getModule('utils/AppInit'),
+		NodeDomain = brackets.getModule('utils/NodeDomain'),
+		ExtensionUtils = brackets.getModule('utils/ExtensionUtils'),
+		WorkspaceManager = brackets.getModule('view/WorkspaceManager'),
+		CommandManager = brackets.getModule('command/CommandManager'),
+		KeyBindingManager = brackets.getModule('command/KeyBindingManager');
 
-        if ( currentDocInfo.text !== '' ) {
-            _sendToDomain(currentDocInfo);
-        }
-    }
+	// Load domain
+	var DomainManager_path = ExtensionUtils.getModulePath(module, 'lib/DomainManager');
+	var DomainManager = new NodeDomain('Br-Ps', DomainManager_path);
 
-    /*
-     * @private
-     * Function to connect to the Domain Manager
-     */
-    function _sendToDomain(currentDocInfo) {
-        _showConsolePanel();
-        
-        // Connect to domain - execute 'main' function
-        DomainManager.exec('main', currentDocInfo)
-            .done(function ( result ) {
-                console.log( result );
-            }).fail(function ( err ) {
-                console.error( "Br-Ps: Failed to execute command 'runJSX' - " + err );
-            });
-    }
+	var panelHTML = require("text!html/console.html"),
+		panel,
+		$panel,
+		$clearBtn,
+		$console,
+		$closeBtn,
+		$psVersion,
+		$executionTime,
+		message;
 
-    /*
-     * @private
-     * Function to show the console in Brackets
-     */ 
-    function _showConsolePanel() {
-        panel.show();
-    }
-    
-    /*
-     * @private
-     * Function to send a message to the console in Brackets
-     */
-    function _sendToConsole( message, type ) {
+	/*
+	 * @private
+	 * Function to validate and get the properties of the current document
+	 */
+	function _validateCurrentDocument() {
+		// Get info of working file
+		var currentDoc = DocumentManager.getCurrentDocument();
+		var currentDocInfo = {
+			path: currentDoc.file.fullPath,
+			isDirty: currentDoc.isDirty,
+			text: currentDoc.getText()
+		};
+
+		if (currentDocInfo.text !== '') {
+			_sendToDomain(currentDocInfo);
+		}
+	}
+
+	/*
+	 * @private
+	 * Function to connect to the Domain Manager
+	 */
+	function _sendToDomain(currentDocInfo) {
+		_showConsolePanel();
+
+		// Connect to domain - execute 'main' function
+		DomainManager.exec('main', currentDocInfo)
+			.done(function (result) {
+				console.log(result);
+			}).fail(function (err) {
+				console.error("Br-Ps: Failed to execute command 'runJSX' - " + err);
+			});
+	}
+
+	/*
+	 * @private
+	 * Function to show the console in Brackets
+	 */
+	function _showConsolePanel() {
+		panel.show();
+	}
+
+	/*
+	 * @private
+	 * Function to send a message to the console in Brackets
+	 */
+	function _sendToConsole(message, type) {
 		try {
-			var message = _filerErrors( message );
+			var message = _filerErrors(message);
 
-			if ( message.indexOf('PSVersion') !== -1 ) {
-				_displayPSVersion( message );
+			if (message.indexOf('PSVersion') !== -1) {
+				_displayPSVersion(message);
 				return true;
 			}
 
-			if ( message.indexOf('ExecutionTime') !== -1 ) {
-				_displayExecutionTime( message );
+			if (message.indexOf('ExecutionTime') !== -1) {
+				_displayExecutionTime(message);
 				return true;
 			}
 
-			if ( message ) {
+			if (message) {
 				$console.append('<li class="' + type + '"><pre>' + message + '</pre></li>');
 				$console.scrollTop = $console.scrollHeight;
 
 				_updateScroll();
 			}
-		} catch( err ) { };
-    }
-    
-    /*
-     * @private
-     * Function to filter the messages to display in the console
-     * @return {string} message to be displayed
-     */
-    function _filerErrors( message ) {
-        if ( message && typeof message == "string" && message.toString().indexOf( 'Br-Ps' ) !== -1 ) {
-            return message;
-        }
-    }
-    
-    /*
-     * @private
-     * Function to display the current Application executing the script in the console
-     */
-    function _displayPSVersion( message ) {        
-        message = message.split( ':' );
-        message = message[message.length - 1];
-        
-        $psVersion.html( message );
-    }
+		} catch (err) {};
+	}
 
-    /*
-     * @private
-     * Function to display the current Application executing the script in the console
-     */
-    function _displayExecutionTime( message ) {        
-        message = message.split( ':' );
-        message = message[message.length - 2] + ': ' + message[message.length - 1];
+	/*
+	 * @private
+	 * Function to filter the messages to display in the console
+	 * @return {string} message to be displayed
+	 */
+	function _filerErrors(message) {
+		if (message && typeof message == "string" && message.toString().indexOf('Br-Ps') !== -1) {
+			return message;
+		}
+	}
 
-        $executionTime.html( message );
-    }
-    
-    /*
-     * @private
-     * Function to clear the console in Brackets
-     */
-    function clearConsole() {
-        $console.html( '' );
-        $executionTime.html( '' );
-    }
-    
-    /*
-     * @private
-     * Function to hide the console in Brackets
-     */
-    function hideConsolePanel() {
-        panel.hide();
-    }
-    
-    /*
-     * @private
-     * Function to update the scroll position to display the last maessage
-     */
-    function _updateScroll(){
-        var element       = document.getElementById("br-ps-console");
-        element.scrollTop = element.scrollHeight;
-    }
+	/*
+	 * @private
+	 * Function to display the current Application executing the script in the console
+	 */
+	function _displayPSVersion(message) {
+		message = message.split(':');
+		message = message[message.length - 1];
 
-    /*
-     * @public
-     * Function to execute when Brackets has loaded
-     */
-    AppInit.appReady(function () {
-        
-        // RunJSX Icon
-        ExtensionUtils.loadStyleSheet(module, "styles/styles.css");
-        $(document.createElement("a"))
-            .attr("id", "br-ps-toolbar-icon")
-            .attr("href", "#")
-            .attr("title", "Brackets to Photoshop")
-            .on("click", _validateCurrentDocument)
-            .appendTo($("#main-toolbar .buttons"));
-        
-        // Set key binding
-        var keyboardShortcut_ID = "brackets-to-photoshop.run";
-        CommandManager.register('Run Brackets-To-Photoshop', keyboardShortcut_ID, _validateCurrentDocument);
-        KeyBindingManager.addBinding(keyboardShortcut_ID, "Cmd-Shift-P");
-        
-        // Create panel
-        panel = WorkspaceManager.createBottomPanel("brackets-to-photoshop.panel", $(panelHTML));
-        
-        // Get HTML items
-        $panel         = $('#br-ps-panel');
-        $clearBtn      = $panel.find('#clearBtn');
-        $console       = $panel.find('.resizable-content ul');
-        $closeBtn      = $panel.find('.close');
-        $psVersion     = $panel.find('#psVersion');
-        $executionTime = $panel.find('#executionTime');
-        
-        // On-clicks
-        $clearBtn.click(clearConsole);
-        $closeBtn.click(hideConsolePanel);
-        
-        /*
-         * Extend console functions to be filtered and displayed in Brackets console
-         * console.log / console.warn / console.error / console.info
-         */
-        var _log   = console.log,
-            _warn  = console.warn,
-            _error = console.error,
-            _info  = console.info;
+		$psVersion.html(message);
+	}
 
-        console.log = function() {
-            var arg = arguments;
+	/*
+	 * @private
+	 * Function to display the current Application executing the script in the console
+	 */
+	function _displayExecutionTime(message) {
+		message = message.split(':');
+		message = message[message.length - 2] + ': ' + message[message.length - 1];
 
-            _sendToConsole(arg[0], 'log');
-            return _log.apply(console, arguments);
-        };
+		$executionTime.html(message);
+	}
 
-        console.warn = function() {
-            var arg = arguments;
+	/*
+	 * @private
+	 * Function to clear the console in Brackets
+	 */
+	function clearConsole() {
+		$console.html('');
+		$executionTime.html('');
+	}
 
-            _sendToConsole(arg[0], 'warn');
-            return _warn.apply(console, arguments);
-        };
+	/*
+	 * @private
+	 * Function to hide the console in Brackets
+	 */
+	function hideConsolePanel() {
+		panel.hide();
+	}
 
-        console.error = function() {
-            var arg = arguments;
+	/*
+	 * @private
+	 * Function to update the scroll position to display the last maessage
+	 */
+	function _updateScroll() {
+		var element = document.getElementById("br-ps-console");
+		element.scrollTop = element.scrollHeight;
+	}
 
-            _sendToConsole(arg[0], 'error');
-            return _error.apply(console, arguments);
-        };
-        
-        console.info = function() {
-            var arg = arguments;
+	/*
+	 * @public
+	 * Function to execute when Brackets has loaded
+	 */
+	AppInit.appReady(function () {
 
-            // Using console.info only to find the Application logged from Domain
-            _sendToConsole(arg[0], 'log');
-            return _info.apply(console, arguments);
-        };
-    });
+		// RunJSX Icon
+		ExtensionUtils.loadStyleSheet(module, "styles/styles.css");
+		$(document.createElement("a"))
+			.attr("id", "br-ps-toolbar-icon")
+			.attr("href", "#")
+			.attr("title", "Brackets to Photoshop")
+			.on("click", _validateCurrentDocument)
+			.appendTo($("#main-toolbar .buttons"));
+
+		// Set key binding
+		var keyboardShortcut_ID = "brackets-to-photoshop.run";
+		CommandManager.register('Run Brackets-To-Photoshop', keyboardShortcut_ID, _validateCurrentDocument);
+		KeyBindingManager.addBinding(keyboardShortcut_ID, "Cmd-Shift-P");
+
+		// Create panel
+		panel = WorkspaceManager.createBottomPanel("brackets-to-photoshop.panel", $(panelHTML));
+
+		// Get HTML items
+		$panel = $('#br-ps-panel');
+		$clearBtn = $panel.find('#clearBtn');
+		$console = $panel.find('.resizable-content ul');
+		$closeBtn = $panel.find('.close');
+		$psVersion = $panel.find('#psVersion');
+		$executionTime = $panel.find('#executionTime');
+
+		// On-clicks
+		$clearBtn.click(clearConsole);
+		$closeBtn.click(hideConsolePanel);
+
+		/*
+		 * Extend console functions to be filtered and displayed in Brackets console
+		 * console.log / console.warn / console.error / console.info
+		 */
+		var _log = console.log,
+			_warn = console.warn,
+			_error = console.error,
+			_info = console.info;
+
+		console.log = function () {
+			var arg = arguments;
+
+			_sendToConsole(arg[0], 'log');
+			return _log.apply(console, arguments);
+		};
+
+		console.warn = function () {
+			var arg = arguments;
+
+			_sendToConsole(arg[0], 'warn');
+			return _warn.apply(console, arguments);
+		};
+
+		console.error = function () {
+			var arg = arguments;
+
+			_sendToConsole(arg[0], 'error');
+			return _error.apply(console, arguments);
+		};
+
+		console.info = function () {
+			var arg = arguments;
+
+			// Using console.info only to find the Application logged from Domain
+			_sendToConsole(arg[0], 'log');
+			return _info.apply(console, arguments);
+		};
+	});
 });
